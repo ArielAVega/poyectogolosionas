28a3a7971cefbb149412e143050edfe2a2f283f0
Ariel A Vega <ariel.vega@educacionit.com>
2023-09-10 07:42:48 -0300

    Actualizacion atributos: Golosiona.java

--- src/model/domain/Golosiona.java
+++ src/model/domain/Golosiona.java
@@ -1,5 +1,21 @@
 package model.domain;
 
 public class Golosiona {
+	protected int codigo;
+	protected String nombre;
+	protected double precio;
+	protected String ingredientes;
+	
+	public Golosiona() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Golosiona(int codigo, String nombre, double precio, String ingredientes) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.ingredientes = ingredientes;
+	}
+	
+	
 }
